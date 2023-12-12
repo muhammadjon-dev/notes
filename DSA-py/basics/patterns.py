1cf116067171307705e3c6e6efb3bbf17d1b0824
Muhammadjon Abdirazzokov <muhammadjona2004@gmail.com>
2023-12-12 23:21:05 +0100

    12 patterns done

--- DSA-py/basics/patterns.py
+++ DSA-py/basics/patterns.py
@@ -1,41 +1,146 @@
 def pat1(n: int):
-    pass
+    for i in range(n):
+        for j in range(n):
+            print('*', end='')
+        print()
+        
+# pat1(6)
 
 def pat2(n: int):
-    pass
+    for i in range(1, n+1):
+        # for j in range(i):
+        #     print('*', end='')
+        # print()
+        
+        print(i*'*')
+
+# pat2(5)
 
 def pat3(n: int):
-    pass
+    for i in range(n):
+        for j in range(i+1): 
+            print(str(j+1), end='')
+        print()
+
+# pat3(6)
 
 def pat4(n: int):
-    pass
+    for i in range(1, n+1):
+        # for j in range(i): 
+        #     print(str(i), end='')
+        # print()
+        
+        print(str(i)*i)
+
+# pat4(5)
 
 def pat5(n: int):
-    pass
+    # for i in range(n, 0, -1):
+    #     for j in range(i):
+    #         print('*', end='')
+    #     print()
+    
+    for i in range(n):
+        print('*'*(n-i))
+        
+
+# pat5(5)
 
 def pat6(n: int):
-    pass
+    # for i in range(n, 0, -1):
+    #     for j in range(1, i+1):
+    #         print(str(j), end='')
+    #     print()
+
+    for i in range(n):
+        for j in range(1, n-i+1):
+            print(str(j), end='')
+        print()
+        
+# pat6(5)
 
 def pat7(n: int):
-    pass
+    for i in range(n):
+        spaces = n-i-1
+        stars=2*i+1
+        print(spaces*' ', end='')
+        # for j in range(2*i+1):
+        #     print('*', end='')
+        print('*'*stars, end='')
+        print(spaces*' ')
+
+# pat7(5)
 
 def pat8(n: int):
-    pass
+    for i in range(n, 0, -1):
+        spaces = n-i
+        stars=2*i-1
+        print(spaces*' ', end='')
+        # for j in range(2*i-1):
+        #     print('*', end='')
+        print('*'*stars, end='')
+        print(spaces*' ')
+
+# pat8(10)
 
 def pat9(n: int):
-    pass
+    # for i in range(n):
+    #     counts = n-i-1
+    #     print(counts*' ', end='')
+    #     for j in range(2*i+1):
+            
+    #         print('*', end='')
+    #     print(counts*' ')    
+    
+    # for i in range(n, 0, -1):
+    #     counts = n-i
+    #     print(counts*' ', end='')
+    #     for j in range(2*i-1):
+            
+    #         print('*', end='')
+    #     print(counts*' ')
+    pat7(n)
+    pat8(n)
+
+# pat9(3)
 
 def pat10(n: int):
-    pass
+    for i in range(1, 2*n):
+        stars = i
+        if stars > 5:
+            stars=n-i%n #2*n-i
+        print('*'*stars)
+        
+# pat10(5)
 
 def pat11(n: int):
-    pass
+    for i in range(n):
+        for j in range(i+1):
+            if (i + j)%2:
+                print(0, end='')
+            else:
+                print(1, end='')
+        print()
 
+# pat11(5)
+        
 def pat12(n: int):
-    pass
+    for i in range(1, n+1):
+        for j in range(i):
+            print(j+1, end='')
+            
+        spaces = 2*n-2*i
+        print(spaces*' ', end='')
+        
+        for j in range(i, 0, -1):
+            print(j, end='')
+        print()
+pat12(4)
 
 def pat13(n: int):
-    pass
+    for i in range(n):
+        for j in range(n):
+            pass
 
 def pat14(n: int):
     pass
