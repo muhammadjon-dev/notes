04620892bb15a77f28ee9acf8c22b4d05a08d9c0
Muhammadjon Abdirazzokov <muhammadjona2004@gmail.com>
2023-12-12 21:46:34 +0100

    initial pattern file

---
+++ DSA-py/basics/patterns.py
@@ -0,0 +1,66 @@
+def pat1(n: int):
+    pass
+
+def pat2(n: int):
+    pass
+
+def pat3(n: int):
+    pass
+
+def pat4(n: int):
+    pass
+
+def pat5(n: int):
+    pass
+
+def pat6(n: int):
+    pass
+
+def pat7(n: int):
+    pass
+
+def pat8(n: int):
+    pass
+
+def pat9(n: int):
+    pass
+
+def pat10(n: int):
+    pass
+
+def pat11(n: int):
+    pass
+
+def pat12(n: int):
+    pass
+
+def pat13(n: int):
+    pass
+
+def pat14(n: int):
+    pass
+
+def pat15(n: int):
+    pass
+
+def pat16(n: int):
+    pass
+
+def pat17(n: int):
+    pass
+
+def pat18(n: int):
+    pass
+
+def pat19(n: int):
+    pass
+
+def pat20(n: int):
+    pass
+
+def pat21(n: int):
+    pass
+
+def pat22(n: int):
+    pass
+
