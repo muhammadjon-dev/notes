b2004c8c33d1e7c2de9597b5712a905ceb0eca9e
Muhammadjon Abdirazzokov <muhammadjona2004@gmail.com>
2023-12-19 21:00:23 +0100

    dsa 19.12.2023

--- DSA-py/recursion/printname.py
+++ DSA-py/recursion/printname.py
@@ -16,13 +16,34 @@ def printtoN(n: int):
     x+=1
     printtoN(n)
     
-printtoN(5)
+# printtoN(5)
 
 def printItoN(i: int, n: int):
-    if x > n:
+    if i > n:
         return
     print(i)
     i+=1
-    printtoN(n)
+    printItoN(n)
     
-printItoN(1, 5)
+# printItoN(1, 5)
+
+def printNto1(n: int):
+    if 0 >= n:
+        return
+    print(n)
+    n-=1
+    printNto1(n)
+    
+# printNto1(5)
+
+sums = 0
+def sumNto1(n: int):
+    if 0 >= n:
+        return
+    global sums
+    sums+=n
+    n-=1
+    sumNto1(n)
+# sumNto1(5)
+
+
