b3ebf56222d5a170db4f5561588966c5af290c06
Muhammadjon Abdirazzokov <muhammadjona2004@gmail.com>
2024-01-05 10:26:02 +0100

    dm pandas completed

--- py/Data_Manipulation_Python/DM_pandas/dmpandas.py
+++ py/Data_Manipulation_Python/DM_pandas/dmpandas.py
@@ -303,3 +303,270 @@ def fill_missings():
     # Print the mean weekly_sales by department and type; fill missing values with 0s; sum all rows and cols
     print(sales.pivot_table(values="weekly_sales", index="department", columns="type", fill_value=0, margins=True))
     
+
+temperatures = pd.read_csv('temperatures.csv', index_col='Unnamed: 0')
+
+def setindex():
+    # Look at temperatures
+    print(temperatures.head())
+
+    # Set the index of temperatures to city
+    temperatures_ind = temperatures.set_index("city")
+
+    # Look at temperatures_ind
+    print(temperatures_ind)
+
+    # Reset the temperatures_ind index, keeping its contents
+    print(temperatures_ind.reset_index())
+
+    # Reset the temperatures_ind index, dropping its contents
+    print(temperatures_ind.reset_index(drop=True))
+    
+
+def subsetting_loc():
+    # Make a list of cities to subset on
+    cities = ["Moscow", "Saint Petersburg"]
+    # Set the index of temperatures to city
+    temperatures_ind = temperatures.set_index("city")
+    # Subset temperatures using square brackets
+    print(temperatures[temperatures["city"].isin(cities)])
+
+    # Subset temperatures_ind using .loc[]
+    print(temperatures_ind.loc[cities])
+    
+def subsetting_mult():
+    # Index temperatures by country & city
+    temperatures_ind = temperatures.set_index(["country", "city"])
+
+    # List of tuples: Brazil, Rio De Janeiro & Pakistan, Lahore
+    rows_to_keep = [("Brazil","Rio De Janeiro"), ("Pakistan","Lahore")]
+
+    # Subset for rows to keep
+    print(temperatures_ind.loc[rows_to_keep])
+
+temperatures_ind = temperatures.set_index(["country", "city"])
+def sorting_index():
+    # Sort temperatures_ind by index values
+    print(temperatures_ind.sort_index())
+
+    # Sort temperatures_ind by index values at the city level
+    print(temperatures_ind.sort_index(level="city"))
+
+    # Sort temperatures_ind by country then descending city
+    print(temperatures_ind.sort_index(level=["country","city"], ascending=[True, False]))
+
+# Sort the index of temperatures_ind
+temperatures_srt = temperatures_ind.sort_index()    
+def slicing():
+
+    # Subset rows from Pakistan to Russia
+    print(temperatures_srt.loc["Pakistan":"Russia"])
+
+    # Try to subset rows from Lahore to Moscow
+    print(temperatures_srt.loc["Lahore": "Moscow"])
+
+    # Subset rows from Pakistan, Lahore to Russia, Moscow
+    print(temperatures_srt.loc[("Pakistan","Lahore"): ("Russia", "Moscow")])
+    
+
+def slicing1():
+    # Subset rows from India, Hyderabad to Iraq, Baghdad
+    print(temperatures_srt.loc[("India", "Hyderabad"): ("Iraq", "Baghdad")])
+
+    # Subset columns from date to avg_temp_c
+    print(temperatures_srt.loc[:, "date": "avg_temp_c"])
+
+    # Subset in both directions at once
+    print(temperatures_srt.loc[("India", "Hyderabad"): ("Iraq", "Baghdad"), "date": "avg_temp_c"])
+    
+def slicing2():
+    # Use Boolean conditions to subset temperatures for rows in 2010 and 2011
+    temperatures_bool = temperatures[(temperatures["date"] >= "2010-01-01") & (temperatures["date"] <= "2011-12-31")]
+    print(temperatures_bool)
+
+    # Set date as the index and sort the index
+    temperatures_ind = temperatures.set_index("date").sort_index()
+
+    # Use .loc[] to subset temperatures_ind for rows in 2010 and 2011
+    print(temperatures_ind.loc["2010": "2011"])
+
+    # Use .loc[] to subset temperatures_ind for rows from Aug 2010 to Feb 2011
+    print(temperatures_ind.loc["2010-08":"2011-02"])
+    
+def slicing3():
+    # Get 23rd row, 2nd column (index 22, 1)
+    print(temperatures.iloc[22, 1])
+
+    # Use slicing to get the first 5 rows
+    print(temperatures.iloc[:5])
+
+    # Use slicing to get columns 3 to 4
+    print(temperatures.iloc[:, 2:4])
+
+    # Use slicing in both directions at once
+    print(temperatures.iloc[:5, 2:4])
+    
+def pivoting():
+    # Add a year column to temperatures
+    temperatures["year"]=temperatures["date"].dt.year
+
+    # Pivot avg_temp_c by country and city vs year
+    temp_by_country_city_vs_year = temperatures.pivot_table(values="avg_temp_c", index=["country", "city"], columns="year")
+
+    # See the result
+    print(temp_by_country_city_vs_year)
+    
+def subsetting_pivot():
+    temp_by_country_city_vs_year = temperatures.pivot_table(values="avg_temp_c", index=["country", "city"], columns="year")
+    # Subset for Egypt to India
+    temp_by_country_city_vs_year.loc["Egypt": "India"]
+
+    # Subset for Egypt, Cairo to India, Delhi
+    temp_by_country_city_vs_year.loc[("Egypt", "Cairo"): ("India", "Delhi")]
+
+    # Subset for Egypt, Cairo to India, Delhi, and 2005 to 2010
+    temp_by_country_city_vs_year.loc[("Egypt", "Cairo"): ("India", "Delhi"), "2005":"2010"]
+    
+def filtering_pivot():
+    temp_by_country_city_vs_year = temperatures.pivot_table(values="avg_temp_c", index=["country", "city"], columns="year")
+    # Get the worldwide mean temp by year
+    mean_temp_by_year = temp_by_country_city_vs_year.mean(axis="index")
+
+    # Filter for the year that had the highest mean temp
+    print(mean_temp_by_year[mean_temp_by_year == mean_temp_by_year.max()])
+
+    # Get the mean temp by city
+    mean_temp_by_city = temp_by_country_city_vs_year.mean(axis="columns")
+
+    # Filter for the city that had the lowest mean temp
+    print(mean_temp_by_city[mean_temp_by_city == mean_temp_by_city.min()])
+    
+# Import matplotlib.pyplot with alias plt
+import matplotlib.pyplot as plt 
+import pickle as pkl
+# visualizing
+with open("avoplotto.pkl", "rb") as f:
+    object = pkl.load(f)
+    
+
+avocados = pd.DataFrame(object)
+
+def make_plot():
+    # Look at the first few rows of data
+    print(avocados.head())
+
+    # Get the total number of avocados sold of each size
+    nb_sold_by_size = avocados.groupby("size")["nb_sold"].sum()
+
+    # Create a bar plot of the number of avocados sold by size
+    nb_sold_by_size.plot(x="size", kind="bar")
+
+    # Show the plot
+    plt.show()
+    
+def make_plot1():
+    # Get the total number of avocados sold on each date
+    nb_sold_by_date = avocados.groupby("date")["nb_sold"].sum()
+
+    # Create a line plot of the number of avocados sold by date
+    nb_sold_by_date.plot(x="date", kind="line")
+
+    # Show the plot
+    plt.show()
+    
+def scatterplot():
+    # Scatter plot of avg_price vs. nb_sold with title
+    avocados.plot(x="nb_sold", y="avg_price", kind="scatter", title="Number of avocados sold vs. average price")
+
+    # Show the plot
+    plt.show()
+    
+def histogram1():
+    # Histogram of conventional avg_price 
+    avocados[avocados["type"]=="conventional"]["avg_price"].hist()
+
+    # Histogram of organic avg_price
+    avocados[avocados["type"]=="organic"]["avg_price"].hist()
+
+    # Add a legend
+    plt.legend(["conventional" , "organic"])
+
+    # Show the plot
+    plt.show()
+    
+def histogram2():
+    # Modify bins to 20
+    avocados[avocados["type"] == "conventional"]["avg_price"].hist(alpha=0.5, bins=20)
+
+    # Modify bins to 20
+    avocados[avocados["type"] == "organic"]["avg_price"].hist(alpha=0.5, bins=20)
+
+    # Add a legend
+    plt.legend(["conventional", "organic"])
+
+    # Show the plot
+    plt.show()
+    
+# missing values
+def missing_graph():
+    # Check individual values for missing values
+    print(avocados.isna())
+
+    # Check each column for missing values
+    print(avocados.isna().any())
+
+    # Bar plot of missing values by variable
+    avocados.isna().sum().plot(kind="bar")
+
+    # Show plot
+    plt.show()
+    
+def drop_missing():
+    # Remove rows with missing values
+    avocados_complete = avocados.dropna()
+
+    # Check if any columns contain missing values
+    print(avocados_complete.isna().any().sum())
+    
+def fill_missing():
+    # From previous step
+    cols_with_missing = ["small_sold", "large_sold", "xl_sold"]
+    avocados[cols_with_missing].hist()
+    plt.show()
+
+    # Fill in missing values with 0
+    avocados_filled = avocados.fillna(0)
+
+    # Create histograms of the filled columns
+    avocados_filled[cols_with_missing].hist()
+
+    # Show the plot
+    plt.show()
+    
+def create_df():
+    # Create a list of dictionaries with new data
+    avocados_list = [
+        {"date": "2019-11-03", "small_sold": 10376832, "large_sold": 7835071},
+        {"date": "2019-11-10", "small_sold": 10717154, "large_sold": 8561348},
+    ]
+
+    # Convert list into DataFrame
+    avocados_2019 = pd.DataFrame(avocados_list)
+
+    # Print the new DataFrame
+    print(avocados_2019)
+
+def create_df1():
+    # Create a dictionary of lists with new data
+    avocados_dict = {
+    "date": ["2019-11-17", "2019-12-01"],
+    "small_sold": [10859987,	9291631],
+    "large_sold": [7674135, 6238096]
+    }
+
+    # Convert dictionary into DataFrame
+    avocados_2019 = pd.DataFrame(avocados_dict)
+
+    # Print the new DataFrame
+    print(avocados_2019)
+    
