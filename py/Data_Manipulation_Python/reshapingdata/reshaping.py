f83af2a27ae5316bfc6ddc4a905617c04a206890
Muhammadjon Abdirazzokov <muhammadjona2004@gmail.com>
2024-01-08 19:51:10 +0100

    chapter 3 done

--- py/Data_Manipulation_Python/reshapingdata/reshaping.py
+++ py/Data_Manipulation_Python/reshapingdata/reshaping.py
@@ -299,3 +299,134 @@ def all_in_one2():
 
     # Print sh_long 
     print(sh_long)
+
+# chapter3
+# multi indexing: pd.MultiIndex.from_array(array_name, names=column_name_for_element_in_array)
+# in short:
+# stacking - changes cols to rows
+# unsracking - makes rows to cols
+
+churn = pd.read_csv('churn-bigml-20.csv')
+def multi_indexing():
+    # Predefined list to use as index
+    new_index = [['California', 'California', 'New York', 'Ohio'], 
+                ['Los Angeles', 'San Francisco', 'New York', 'Cleveland']]
+
+    # Create a multi-level index using predefined new_index
+    churn_new = pd.MultiIndex.from_arrays(new_index, names=['state', 'city'])
+
+    # Assign the new index to the churn index
+    churn.index = churn_new
+
+    # Reshape by stacking churn DataFrame
+    churn_stack = churn.stack()
+
+    # Print churn_stack
+    print(churn_stack)
+    
+def stacking_level():
+    # Set state and city as index modifying the DataFrame
+    churn.set_index(['state', 'city'], inplace=True)
+
+    # Reshape by stacking the second level
+    churn_stack = churn.stack(level=1)
+
+    # Print churn_stack
+    print(churn_stack)
+    
+def stacking_col():
+    # Stack churn by the feature column level
+    churn_feature = churn.stack(level='feature')
+
+    # Print churn_feature
+    print(churn_feature)
+    
+def unstacking():
+    # Reshape the churn DataFrame by unstacking
+    churn_unstack = churn.unstack()
+
+    # Print churn_unstack
+    print(churn_unstack)
+    
+    # Reshape churn by unstacking the first row level
+    churn_first = churn.unstack(level=0)
+
+    # Print churn_zero
+    print(churn_first)
+    
+    # Reshape churn by unstacking the second row level
+    churn_second = churn.unstack(level=1)
+
+    # Print churn_second
+    print(churn_second)
+    
+def unstacking_row():
+    # Sort the index in descending order
+    churn_time = churn.unstack(level='time').sort_index(ascending=False)
+
+    # Print churn_time
+    print(churn_time)
+    
+def unstacking_stacking():
+    # Unstack churn by type level
+    churn_type = churn.unstack(level='type')
+
+    # Stack the resulting DataFrame using the first column level
+    churn_final = churn_type.stack(level=0)
+
+    # Print churn_type
+    print(churn_final)
+    
+#swap levels
+# df.swaplevel(0, 2)
+# df.swaplevel(0, 1, axis=1) swaps column levels
+
+def swapping1():
+    # Switch the first and third row index levels in churn
+    churn_swap = churn.swaplevel(0, 2)
+
+    # Reshape by unstacking the last row level 
+    churn_unstack = churn_swap.unstack()
+
+    # Print churn_unstack
+    print(churn_unstack)
+    
+def swapping2():
+    # Unstack the first and second row level of churn
+    churn_unstack = churn.unstack(level=[0, 1])
+
+    # Stack the resulting DataFrame using plan and year
+    churn_py = churn_unstack.stack(['plan', 'year'])
+
+    # Switch the first and second column levels
+    churn_switch = churn_py.swaplevel(0, 1, axis=1)
+
+    # Print churn_switch
+    print(churn_switch)
+    
+# handling missing values
+# .unstack(level='class', fill_value='No')
+# .stack(dropna=True) - default
+# .stack(dropna=False).fillna(0)
+
+def fill_values1():
+    # Unstack churn level and fill missing values with zero
+    churn = churn.unstack(level='churn', fill_value=0)
+
+    # Sort by descending voice mail plan and ascending international plan
+    churn_sorted = churn.sort_index(level=['voice_mail_plan', 'international_plan'], 
+                            ascending=[False, True])
+
+    # Print final DataFrame and observe pattern
+    print(churn_sorted)
+    
+def fill_values2():
+    # Stack the level scope without dropping rows with missing values
+    churn_stack = churn.stack(level='scope', dropna=False)
+
+    # Fill the resulting missing values with zero
+    churn_fill = churn_stack.fillna(0)
+
+    # Print churn_fill
+    print(churn_fill)
+    
